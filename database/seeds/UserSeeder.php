8860633dff5f5b9530bb19dcc03f7d134710c4ca
alladinMelico <melico.alladin@gmail.com>
2020-08-23 17:38:07 +0800

    added 'is_admin' => true in seeder of user

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,7 +17,8 @@ class UserSeeder extends Seeder
         	'name'=>'admin',
         	'email'=> 'admin@example.com',
         	'email_verified_at'=> now(),
-        	'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'is_admin' => true,
         	'remember_token' => Str::random(10),
         ]);
 
